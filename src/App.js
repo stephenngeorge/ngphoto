9a98d374810b9e4dc41734c4d46c8ff5330829bc
Stephen Pal-George <stephenngeorge@gmail.com>
2020-03-08 18:01:58 +0000

    setup page component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { useEffect } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import firebase from 'firebase/app'
 import 'firebase/database'
 
-import { Title } from 'loris-ui.portfolio'
+import { Page } from 'loris-ui.portfolio'
+import {
+  FACEBOOK_LOGO,
+  NGPHOTO_LOGO_MARK,
+  SENT_MAIL_ICON,
+  TWITTER_LOGO
+} from './assets'
 
 const App = () => {
   useEffect(() => {
@@ -19,9 +26,37 @@ const App = () => {
   }, [])
 
   return (
-    <div className="app">
-      <Title titleLevel={1} titleText="Heading" />
-    </div>
+    <Router>
+      <div className="app">
+        <Page
+          accreditationLink={{ url: "https://github.com/stephenngeorge", text: "Stephen Pal-George" }}
+          accreditationText="Designed and built by "
+          iconsListData={{
+            direction: "row",
+            icons: [
+              { name: 'facebook', size: 'sm', src: FACEBOOK_LOGO, url: 'https://www.facebook.com' },
+              { name: 'twitter', size: 'sm', src: TWITTER_LOGO, url: 'https://twitter.com' },
+              { name: 'email', size: 'sm', src: SENT_MAIL_ICON, url: 'https://google.co.uk' }
+            ] 
+          }}
+          menuLinks={[
+            { path: "/galleries", label: "galleries" },
+            { path: "/buy", label: "buy" },
+            { path: "/contact", label: "contact" }
+          ]}
+          siteMapLinks={[
+            { path: "/", label: "Home" },
+            { path: "/buy", label: "Buy" },
+            { path: "/galleries", label: "Galleries" },
+            { path: "/contact", label: "Contact" }
+          ]}
+          siteLogo={ NGPHOTO_LOGO_MARK }
+          siteTitle="Neil George Photography"
+        >
+
+        </Page>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import FACEBOOK_LOGO from './facebook-logo.svg'
+import NGPHOTO_LOGO_MARK from './NGPHOTO_LOGO_MARK.svg'
+import SENT_MAIL_ICON from './sent-mail.svg'
+import TWITTER_LOGO from './twitter-logo.svg'
+
+export {
+  FACEBOOK_LOGO,
+  NGPHOTO_LOGO_MARK,
+  SENT_MAIL_ICON,
+  TWITTER_LOGO
+}
